2b15fe6a72602446d30615e75ec1663e366bae8a
MaxGamesInc <sibghat.sonu@gmail.com>
2023-12-21 17:45:35 +0500

    Attributes Section

--- Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -2,4 +2,43 @@
 
 
 #include "AbilitySystem/AuraAttributeSet.h"
+#include "Net/UnrealNetwork.h"
 
+
+
+
+
+
+UAuraAttributeSet::UAuraAttributeSet()
+{
+    InitHealth(100.f);
+    InitMaxHealth(100.f);
+    InitMagic(250.f);
+    InitMaxMagic(250.f);
+}
+
+void UAuraAttributeSet::GetLifetimeReplicatedProps(TArray<FLifetimeProperty> &OutLifetimeProps) const
+{
+     Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Health,COND_None,REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxHealth,COND_None,REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,Magic,COND_None,REPNOTIFY_Always);
+    DOREPLIFETIME_CONDITION_NOTIFY(UAuraAttributeSet,MaxMagic,COND_None,REPNOTIFY_Always);
+}
+void UAuraAttributeSet::OnRep_Health(const FGameplayAttributeData OldHealth) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Health,OldHealth);
+}
+void UAuraAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData OldMaxHealth) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxHealth,OldMaxHealth);
+}
+void UAuraAttributeSet::OnRep_Magic(const FGameplayAttributeData OldMagic) const
+{
+   GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,Magic,OldMagic);  
+}
+
+void UAuraAttributeSet::OnRep_MaxMagic(const FGameplayAttributeData OldMaxMagic) const
+{
+    GAMEPLAYATTRIBUTE_REPNOTIFY(UAuraAttributeSet,MaxMagic,OldMaxMagic);
+}
--- Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
+++ Source/Aura/Public/AbilitySystem/AuraAttributeSet.h
@@ -4,7 +4,16 @@
 
 #include "CoreMinimal.h"
 #include "AttributeSet.h"
+#include "GameFramework/Actor.h"
+#include "AbilitySystemComponent.h"
 #include "AuraAttributeSet.generated.h"
+
+class FLifeTimeProperty;
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+ 	GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+ 	GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+	GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+ 	GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
 
 /**
  * 
@@ -13,5 +22,30 @@ UCLASS()
 class AURA_API UAuraAttributeSet : public UAttributeSet
 {
 	GENERATED_BODY()
-	
+
+public:
+	UAuraAttributeSet();
+	virtual void GetLifetimeReplicatedProps( TArray< FLifetimeProperty > & OutLifetimeProps) const override;
+	UFUNCTION()
+	void OnRep_Health(const FGameplayAttributeData OldHealth) const;
+	UFUNCTION()
+	void OnRep_MaxHealth(const FGameplayAttributeData OldMaxHealth) const;
+	UFUNCTION()
+	void OnRep_Magic(const FGameplayAttributeData OldMagic) const;
+	UFUNCTION()
+	void OnRep_MaxMagic(const FGameplayAttributeData OldMaxMagic) const;
+
+
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Health,Category = "Vital Attributes")
+	FGameplayAttributeData Health;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Health)
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_MaxHealth,Category = "Vital Attributes")
+	FGameplayAttributeData MaxHealth;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxHealth)
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Magic,Category = "Vital Attributes")
+	FGameplayAttributeData Magic;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,Magic)
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_MaxMagic,Category = "Vital Attributes")
+	FGameplayAttributeData MaxMagic;
+	ATTRIBUTE_ACCESSORS(UAuraAttributeSet,MaxMagic)
 };
